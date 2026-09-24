473238717cf2b2640a51260d7a2366530b0bcb08
Abinayan Ramalingam <abinayanramalingam2020@gmail.com>
2026-09-24 09:34:04 +0530

    Connect frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@
 // CONFIGURATION
 // ======================================================
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://magic-dine-restaurant-ai.onrender.com";
 
 
 // ======================================================
